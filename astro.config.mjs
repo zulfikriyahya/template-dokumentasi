08384ed66817faede2456dc69ab0c3a94263b41e
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-11-08 20:06:00 +0700

    Initial commit from Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,134 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeRapide from "starlight-theme-rapide";
+import expressiveCode from "astro-expressive-code";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://docs.zedlabs.id",
+
+  integrations: [
+    starlight({
+      plugins: [starlightThemeRapide()],
+
+      expressiveCode: {
+        // You can use any of the themes bundled with Shiki by name,
+        // specify a path to JSON theme file, or pass an instance
+        // of the `ExpressiveCodeTheme` class here:
+        themes: ["github-dark-default", "solarized-light"],
+        shiki: {
+          // You can pass additional plugin options here,
+          // e.g. to load custom language grammars:
+          langs: [
+            // import('./some-exported-grammar.mjs'),
+            // JSON.parse(fs.readFileSync('./some-json-grammar.json', 'utf-8'))
+          ],
+          // bundledLangs: ["astro", "sass"],
+        },
+        frames: {
+          // Example: Hide the "Copy to clipboard" button
+          showCopyToClipboardButton: true,
+        },
+        styleOverrides: {
+          // You can optionally override the plugin's default styles here
+          textMarkers: {
+            // Make default marker color slightly purple
+            markHue: "310",
+            // Reduce marker border opacity
+            borderOpacity: "50%",
+          },
+          // You can optionally override the plugin's default styles here
+          frames: {
+            shadowColor: "#124",
+          },
+        },
+        defaultProps: {
+          // Enable word wrap by default
+          wrap: true,
+          // Disable wrapped line indentation for terminal languages
+          overridesByLang: {
+            "bash,ps,sh": { preserveIndent: false },
+          },
+        },
+      },
+
+      title: "Dokumentasi Aplikasi Madrasah",
+
+      customCss: [
+        // Fontsource files for to regular and semi-bold font weights.
+        "@fontsource/lexend/400.css",
+        "@fontsource/lexend/600.css",
+        "./src/styles/custom.css",
+      ],
+
+      // disable404Route: true,
+
+      // editLink: {
+      //   baseUrl: "https://github.com/withastro/starlight/edit/main/docs/",
+      // },
+
+      tableOfContents: true,
+      // tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 2 },
+
+      logo: {
+        // src: "./src/assets/logo.png",
+        replacesTitle: true,
+        dark: "./src/assets/brand-darkmode.png",
+        light: "./src/assets/brand-lightmode.png",
+      },
+
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+        {
+          icon: "linkedin",
+          label: "Linkedin",
+          href: "https://github.com/withastro/starlight",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://astro.build/chat",
+        },
+        {
+          icon: "instagram",
+          label: "Instagram",
+          href: "https://github.com/withastro/starlight",
+        },
+        {
+          icon: "facebook",
+          label: "Facebook",
+          href: "https://github.com/withastro/starlight",
+        },
+        {
+          icon: "twitter",
+          label: "Twitter",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
