2e907f5d3e00113cb565dbf05e0fa4644f5b173a
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-11-10 17:54:49 +0700

    update

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+module.exports = {
+  semi: false,               // Tidak pakai titik koma di akhir baris
+  singleQuote: true,         // Gunakan tanda kutip tunggal
+  jsxSingleQuote: true,      // Gunakan kutip tunggal di JSX
+  printWidth: 80,            // Maksimal lebar baris 80 karakter
+  tabWidth: 2,               // Indentasi 2 spasi
+  trailingComma: 'es5',      // Tambahkan koma di akhir objek/array multiline
+  bracketSpacing: true,      // Spasi di dalam kurung kurawal
+  arrowParens: 'avoid',      // Hindari tanda kurung pada parameter tunggal arrow function
+}
